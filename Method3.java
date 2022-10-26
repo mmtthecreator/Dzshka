3a2060aee5ec1be1128b0c2d2f734006ab957a37
mmtthecreator <115922088+mmtthecreator@users.noreply.github.com>
2022-10-26 20:41:21 +0300

    Dz

--- Method3.java
+++ Method3.java
@@ -1,13 +1,16 @@
 public class Method3 {
     public static void salarycalc(float years, float salaryNow){
+        float salaryUpd = 0;
         if(years >=1 && years <=3){
-            float salaryUpd = salaryNow + (salaryNow * 10 / 100);
-            System.out.println("Your updated salary: " +salaryUpd);
-        }else if(years > 3){
-            float salaryUpd = salaryNow + (salaryNow * 20 / 100);
-            System.out.println("Your updated salary: " +salaryUpd);
-        }else{
-            System.out.println("Your experience less than 1 year!");
-    }
+            salaryUpd = salaryNow + (salaryNow * 10 / 100);
+        }if(years > 3){
+            salaryUpd = salaryNow + (salaryNow * 20 / 100);
+        }if(salaryUpd < 4000){
+            float salaryBonus = salaryUpd + 1000;
+            System.out.println("Your updated salary: " + salaryBonus);
+    }else{ 
+            float salaryBonus = salaryUpd + 500;
+            System.out.println("Your updated salary: " + salaryBonus);
+        }
 }
 }
